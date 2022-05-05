e9569bf31b54d54faca3fcb4f1cbbe9e1d9eeedf
WaterGuo1 <WaterGuo1@gmail.com>
2022-05-05 17:15:47 +0800

    添加全局样式和局部变量

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,21 @@
 <template>
-  <div> 
+  <div id="app"> 
     <router-view/>
-
-
   </div>
   
 </template>
 
 <style lang="scss">
+@import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 
-*{margin: 0;padding: 0;
-box-sizing: border-box;
-
-}
-
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body{
+  line-height: 1.5;
+  font-family: $font-hei;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-a{
-text-decoration: none;
-color:inherit;
+  color: #333;
+
 }
 
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
