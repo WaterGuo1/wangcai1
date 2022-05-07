1dffeb8bfdeccd056a8f139fa3f925468f0a1a9f
WaterGuo1 <WaterGuo1@gmail.com>
2022-05-07 21:11:11 +0800

    写完css和html

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,24 +1,36 @@
 <template>
-    <div>
-  <Layout>
-<p>money.vue</p>
-<p>money.vue</p>
-<p>money.vue</p>
+    
+  <Layout class-prefix="layout">
+ <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
 
   </Layout>
 
-    </div>
-   
+       
 </template>
 
  <script lang= "ts">
- 
+ import NumberPad from '@/components/Money/NumberPad.vue'
+import Types from '@/components/Money/Types.vue'
+
+ import Notes from '@/components/Money/Notes.vue'
+
+ import Tags from '@/components/Money/Tags.vue'
+
  export default {
      name:'Money',
+     components:{
+       Tags,Notes,Types,NumberPad
+     },
      
  };
  </script>
+ <style lang="scss">
+  .layout-content {
+    display: flex;
+    flex-direction: column-reverse;
+  }
+</style>
 
- <style lang= "scss" scoped>
- 
- </style>
