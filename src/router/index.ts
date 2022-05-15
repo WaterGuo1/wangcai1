cf83a6a9f1731b4d5190bf9394eef8eb0e69b62c
WaterGuo1 <WaterGuo1@gmail.com>
2022-05-15 18:42:20 +0800

    新建一个标签后在地址后面加上/1 跳转无效

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,7 +33,7 @@ const routes: Array<RouteConfig> = [
   component:Statistics
 },
 {
-path:'/labels/edit',
+path:'/labels/edit/:id',
 component:EditLabel
 
 },
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,11 +6,25 @@
 </template>
 
  <script lang= "ts">
-import Layout from '@/components/Layout.vue';
- 
- export default {
-  components: { Layout },
-     name:'EditLabel',
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { model as tagListModel } from '@/models/tagListModel';
+
+@Component
+
+ export default class EditLabel extends Vue{
+ created(){
+        const id = this.$route.params.id;
+        tagListModel.fetch();
+        const tags =tagListModel.data;
+        const tag =  tags.filter(t => t.id ===id)[0];
+        if(tag){
+          console.log(tag)
+        }else{
+          this.$router.replace('/404')
+        }
+
+ }
      
  };
  </script>
