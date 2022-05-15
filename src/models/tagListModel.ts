920192d95718a5291fb461e8240522f4184fa233
WaterGuo1 <WaterGuo1@gmail.com>
2022-05-15 20:28:36 +0800

    消除了大小写拼写错误，其他的报错（包括上次提交的bug），仍未解决

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,38 +1,38 @@
 import Tags from '../components/Money/Tags.vue';
 const localStorageKeyName = 'tagList';
-type Tag={
-id:string;
-name:string;
+type Tag = {
+    id: string;
+    name: string;
 
 }
 
-type TagListModel= {
-data:Tag[]
-fetch:()=>Tag[]
-create:(name:string)=>'success'|'duplicated'
-save:()=>void
+type TagListModel = {
+    data: Tag[]
+    fetch: () => Tag[]
+    create: (name: string) => 'success' | 'duplicated'
+    save: () => void
 
 }
 const model = {
-    data:[],
+    data: [],
 
-fetch(){
-this.data=JSON.parse(window.localStorage.getItem('localStorageKeyName')||'[]') ;
-return this.data
-},
-create(name:string){
-    const names= this.data.map(item =>item.name)
-    if(names.indexOf(name)>=0){return 'duplicated';}
-    this.data.push({id:name,name:name});
-    this.save();
-    return 'success'
-        },
-save (data:Recorditem[]){
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+    fetch() {
+        this.data = JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]');
+        return this.data
+    },
+    create(name: string) {
+        const names = this.data.map(item => item.name)
+        if (names.indexOf(name) >= 0) { return 'duplicated'; }
+        this.data.push({ id: name, name: name });
+        this.save();
+        return 'success'
+    },
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
 
-},
+    },
 
 
 
 };
-export{model}
+export { model }
